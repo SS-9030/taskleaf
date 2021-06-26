8481a7c82d70449b98d4b9662f52e172470202e8
S.S <tksg7982@gmail.com>
2021-06-26 11:30:19 +0900

    taskleafの更新

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -14,10 +14,25 @@ class TasksController < ApplicationController
   def edit
   end
 
+  def update
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
+  end
+
   def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "タスク 「#{task.name}」を登録しました。"
+    @task = current_user.tasks.new(task_params)
+
+    if @task.save
+      logger.debug "task: #{@task.attributes.inspect}"
+      redirect_to @task, notice: "タスク 「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end  
   end
   
   private
@@ -25,4 +40,8 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:name, :description)
   end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end  
 end
